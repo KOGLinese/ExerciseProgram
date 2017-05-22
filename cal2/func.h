94afc674837a0bf59722f4b97530ec965f2cd3e6
KOGLinese <1076452761@qq.com>
2017-05-22 19:06:53 +0800

    第一次转型

--- cal2/func.h
+++ cal2/func.h
@@ -7,9 +7,9 @@
 #include <string>
 #include <cmath>
 using namespace std;
-int IsOperator(char c);
-int Priority(char oper1, char oper2);
-double calculate(double a, int oper, double b);
+int IsOperator(char c);// 判断是否为运算符 
+int Priority(char oper1, char oper2);//运算符比较 
+double calculate(double a, int oper, double b);//两个数的计算 
 double calculateResult(char *express);//用于计算结果
 char change(int x);//随机数转变成运算符 
 char randomNumber();//用于随机生成数字
@@ -21,4 +21,5 @@ void print(double r, double sum);
 void writeFile(char *s, char * p, double rans, double inans);
 int readFile(char * s);
 double scanf2(char * p, int i);//输入答案
+void Choose(char c, int a);//语言选择 
 void writeFile2(char *s,double n,double right);
