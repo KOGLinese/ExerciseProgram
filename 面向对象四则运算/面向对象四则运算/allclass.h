5dfde1ab49d133ba5474187a246a7f2ba78eb619
KOGLinese <1076452761@qq.com>
2017-05-22 23:44:24 +0800

    添加部分注释

--- 面向对象四则运算/面向对象四则运算/allclass.h
+++ 面向对象四则运算/面向对象四则运算/allclass.h
@@ -8,26 +8,26 @@
 #include <cmath>
 using namespace std;
 class expresses {
-private:
-	char a[4];
-	char o[3];
+	private:
+	char a[4];//数字
+	char o[3];//运算符
 	char change(int x);
-	char randomNumber();
-	char randomOperation();
-	int IsOperator(char c);
-	int Priority(char oper1, char oper2);
-	double calculate(double a, int oper, double b);
-public:
-	double calculateResult(char * express);
-	bool IsInt(double answer);
-	char * generateExpression();
+	char randomNumber();//随机数
+	char randomOperation();//随机运算符
+	int IsOperator(char c);//内部判断否为运算符
+	int Priority(char oper1, char oper2);//比较优先级
+	double calculate(double a, int oper, double b);//计算
+	public:
+	double calculateResult(char * express);//计算表达式
+	bool IsInt(double answer);//判断答案是否为整数
+	char * generateExpression();//表达式
 };
 
 class Language {
-private:
+	private:
 	char language;
 	ifstream inf;//读取语言文件
-public:
+	public:
 		void set(char c);//外界传入
 		void scanf();//初始化界面'
 		void scanf2(char * p, int i);
@@ -36,15 +36,15 @@ public:
 };
 class R_and_W//读取类
 {
-private:
+	private:
 	int n;//题数传入
 	ifstream fin;//读取题数文件
-	ofstream fout;
+	ofstream fout;//文件流
 	double inputanswer;
-public :
+	public :
 	void set(double ans);
-	int readFile(char *s);
-	void writeFile1(char *s, char * p, double rans, double inans);
-	void writeFile2(char *s, double right);
-	int Countright(double answers, double inputanswer, Language &l);
+	int readFile(char *s);//读取题数
+	void writeFile1(char *s, char * p, double rans, double inans);//每次情况 存入out
+	void writeFile2(char *s, double right);//情况总结存入
+	int Countright(double answers, double inputanswer, Language &l);//计数
 };
